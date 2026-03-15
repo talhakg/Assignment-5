7264b3221f4dbf42289cbad62b6651e8b4b34682
Talha Ghori <talhaghori18@gmail.com>
2026-03-15 11:28:33 -0400

    task 1 completed

---
+++ script.js
@@ -0,0 +1,17 @@
+function weather(month, day) {
+
+    if ((month == 12 && day >= 15) || month <= 2 || (month == 3 && day <= 25)) {
+        return "The current season is Winter"; }
+
+    if (month == 4 || month == 5) {
+        return "The current season is Spring"; }
+
+    if ((month >= 6 && month <= 8) || (month == 9 && day <= 15)) {
+        return "The current season is Summer"; }
+
+    if ((month >= 10 && month <= 11) || (month == 9 && day >= 16)) {
+        return "The current season is Fall"; }
+
+    return "Unknown"; }
+
+    
